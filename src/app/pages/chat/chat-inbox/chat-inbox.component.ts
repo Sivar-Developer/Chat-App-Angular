2a0f89c9c642b35c59ad1f40c9025a1e191e3d18
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-07-28 18:31:16 +0300

    Chat Inbox Layout

--- src/app/pages/chat/chat-inbox/chat-inbox.component.ts
+++ src/app/pages/chat/chat-inbox/chat-inbox.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var require: any
+const data: any = require('./data.json')
 
 @Component({
   selector: 'app-chat-inbox',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatInboxComponent implements OnInit {
 
-  constructor() { }
+  dialogs = data
+  activeIndex = 0
+  name = this.dialogs[this.activeIndex].name
+  position = this.dialogs[this.activeIndex].position
+  dialog = this.dialogs[this.activeIndex].dialog
+  avatar = this.dialogs[this.activeIndex].avatar
 
-  ngOnInit(): void {
+  constructor() {}
+  ngOnInit() {}
+
+  changeDialog(index) {
+    this.activeIndex = index
+    this.name = this.dialogs[index].name
+    this.position = this.dialogs[index].position
+    this.dialog = this.dialogs[index].dialog
+    this.avatar = this.dialogs[index].avatar
   }
 
 }
--- src/app/pages/chat/chat.module.ts
+++ src/app/pages/chat/chat.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
 import { SharedModule } from 'src/app/shared.module';
 import { ChatRoutingModule } from './chat-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 
 
 @NgModule({
   declarations: [ChatInboxComponent],
   imports: [
-    SharedModule, ChatRoutingModule, FormsModule, ReactiveFormsModule
+    SharedModule, ChatRoutingModule, FormsModule, ReactiveFormsModule, PerfectScrollbarModule
   ]
 })
 export class ChatModule { }
